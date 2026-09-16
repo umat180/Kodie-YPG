6b492f27ef7128c2444b5692701da66f9a4160d5
umat180 <da-pokyere1924@st.umat.edu.gh>
2026-09-16 09:43:47 -0700

    Delete copy_logo.ps1

--- copy_logo.ps1
+++
@@ -1,2 +0,0 @@
-Copy-Item 'C:\Users\hp\Downloads\Kbee.jpeg' 'C:\Users\hp\Desktop\krobo ypg\logo.png' -Force
-Write-Host ('logo.png -> ' + (Get-Item 'C:\Users\hp\Desktop\krobo ypg\logo.png').Length + ' bytes')
