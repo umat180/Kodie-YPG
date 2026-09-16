e19d344480d6ef8f99bfe846b2f47a29eab5a861
umat180 <da-pokyere1924@st.umat.edu.gh>
2026-09-16 09:32:59 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "kodie-ypg-v1";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./logo.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/api")) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          if (response.ok && event.request.method === "GET") {
+            const clone = response.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
